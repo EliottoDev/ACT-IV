48b484760e7f532d39a2848e85106eb7f4c05488
Elias D.E. <61325793+EliottoDev@users.noreply.github.com>
2024-12-18 22:36:58 +0100

    Added CLI args parsing logic
    In src/main.rs, a struct named Args was added to handle subcommands using the Clap library. To facilitate this, an enum called Commands was created to represent each subcommand, with each entry corresponding to a specific subcommand. For the default case (when only "act" is typed), a None variant is used to represent the absence of a subcommand.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+// CLI args
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Args {
+    // Subcommand delegator
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    // TODO: Add subcommands
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // TODO: Implement logic
+    match &args.command {
+        None => {}
+        _ => {}
+    }
 }
