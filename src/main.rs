09794319af5f5141e26ca4468e2439dead2b3d7e
Elias D.E. <elias.dinarett@gmail.com>
2024-12-20 21:08:46 +0100

    Add more logic for new commands

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-mod lib;
-
-use clap::{Parser, Subcommand};
-
-// CLI args
-#[derive(Parser, Debug)]
-#[clap(version, about, long_about = None)]
-struct Args {
-    #[clap(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    // TODO: Add subcommands
-    #[clap(about = "Gets project act file info", long_about = None, name = "info")]
-    Info,
-}
+use clap::Parser;
+mod cli;
+mod library;
 
 fn main() {
-    let args = Args::parse();
+    let args = cli::Args::parse();
+    use cli::Commands;
 
-    // TODO: Implement logic
     match &args.command {
-        None => {},
-        _ => {},
+        Commands::Info(path) => cli::info(path),
+        Commands::Throw => cli::throw(),
+        Commands::Catch => cli::catch(),
+        Commands::Wind => cli::wind()
     }
 }
